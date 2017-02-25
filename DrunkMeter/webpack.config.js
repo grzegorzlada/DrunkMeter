a7a1f641848e879bb6a8b86a16451cafeb8a6adb
Grzegorz Lada <grzegorz.lada@gmail.com>
2017-02-25 13:07:36 +0100

    Config updates, moving them around

--- DrunkMeter/webpack.config.js
+++ DrunkMeter/webpack.config.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = {
-    entry: './main.jsx',
+    entry: './js/main.jsx',
     output: {
-        path: __dirname,
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
