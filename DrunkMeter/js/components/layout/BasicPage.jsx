1f0a6497c0b529f2e436468aa8892bad9dd870fd
Grzegorz Lada <grzegorz.lada@gmail.com>
2017-02-26 17:28:31 +0100

    Fixed header and drawer layout

--- DrunkMeter/js/components/layout/BasicPage.jsx
+++ DrunkMeter/js/components/layout/BasicPage.jsx
@@ -6,11 +6,13 @@ export default class BasicPage extends React.Component {
 
     render() {
         return (
-            <div className="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-                <Header title={this.props.children.props.route.title}/>
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-drawer">
+                <Header title={this.props.children.props.route.title} />
                 <Drawer />
                 <main className="mdl-layout__content mdl-color--grey-100">
-                    {this.props.children}
+                    <div className="page-content">
+                        {this.props.children}
+                    </div>
                 </main>
             </div>
         );
--- DrunkMeter/js/components/layout/Header.jsx
+++ DrunkMeter/js/components/layout/Header.jsx
@@ -4,21 +4,11 @@ import {Link} from 'react-router';
 export default class App extends React.Component {
 
     render() {
-
         return (
-            <header className="demo-header mdl-layout__header">
+            <header className="mdl-layout__header">
                 <div className="mdl-layout__header-row">
                     <span className="mdl-layout-title">{this.props.title}</span>
                     <div className="mdl-layout-spacer"></div>
-                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-                        <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="search">
-                            <i className="material-icons">search</i>
-                        </label>
-                        <div className="mdl-textfield__expandable-holder">
-                            <label className="mdl-textfield__label" htmlFor="search">Enter your query...</label>
-                            <input className="mdl-textfield__input" type="text" id="search"></input>
-                        </div>
-                    </div>
                     <button className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
                         <i className="material-icons">more_vert</i>
                     </button>
@@ -31,5 +21,4 @@ export default class App extends React.Component {
             </header>
         );
     }
-
 }
