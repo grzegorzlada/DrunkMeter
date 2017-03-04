2a048f3ee7e19f23536e3732d6cbd7d817b07bc4
Grzegorz Lada <grzegorz.lada@gmail.com>
2017-03-04 18:18:15 +0100

    Extended custom textfield control, added lodash.

--- DrunkMeter/js/components/inputs/TextField.jsx
+++ DrunkMeter/js/components/inputs/TextField.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import {uniqueId} from 'lodash';
 
 const propTypes = {
-    id: React.PropTypes.string.isRequired,
     label: React.PropTypes.string.isRequired,
     validationMessage: React.PropTypes.string.isRequired,
     isInitiallyValid: React.PropTypes.bool,
-    value: React.PropTypes.string,
-    pattern: React.PropTypes.string
+    value: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number
+    ]),
+    pattern: React.PropTypes.string,
+    onChange: React.PropTypes.func
 };
 
 export default class TextField extends React.Component {
     constructor(props) {
         super(props);
+        this.Id = uniqueId('textfield_');
     }
 
     componentWillMount() {
@@ -32,11 +37,30 @@ export default class TextField extends React.Component {
         return cssClasses;
     }
 
+    textfieldChanhge(e) {
+        var value = e.target.value;
+        var regex = new RegExp(this.props.pattern);
+        var isValid = regex.test(value);
+        this.setState({
+            value: value,
+            isValid: isValid
+        });
+
+        if (typeof this.props.onChange !== 'undefined' && this.props.onChange !== null) {
+            this.props.onChange({
+                value: value,
+                isValid: isValid
+            });
+        }
+
+    }
+
     render() {
         return (
             <div className={this.getClassName()}>
-                <input className="mdl-textfield__input" type="text" id={this.props.id} pattern={this.props.pattern} />
-                <label className="mdl-textfield__label" htmlFor={this.props.id}>{this.props.label}</label>
+                <input className="mdl-textfield__input" type="text" id={this.Id} pattern={this.props.pattern}
+                    onChange={(e) => { this.textfieldChanhge(e); }} value={this.state.value} />
+                <label className="mdl-textfield__label" htmlFor={this.Id}>{this.props.label}</label>
                 <span className="mdl-textfield__error">{this.props.validationMessage}</span>
             </div>
         );
--- DrunkMeter/js/components/settings/UserProfile.jsx
+++ DrunkMeter/js/components/settings/UserProfile.jsx
@@ -1,14 +1,65 @@
 import React from 'react';
-import {Button, IconButton, Card, CardTitle, CardText, CardActions, CardMenu} from 'react-mdl';
+import {
+    Card,
+    CardTitle,
+    CardText,
+    RadioGroup,
+    Radio
+} from 'react-mdl';
+import TextField from '../inputs/TextField';
 
 export default class UserProfile extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            sex: 'male',
+            height: {value: 180},
+            weight: {value: 90}
+        };
+    }
+
+    onWeightChange(newWeight) {
+        this.setState({weight: newWeight});
+    }
+
+    onHeightChange(newHeight) {
+        this.setState({height: newHeight});
+    }
+
+    isStateValid() {
+        return this.state.height.isValid && this.state.weight.isValid;
+    }
+
     render() {
         return (
-            <Card shadow={0} style={{width: '100%', margin: 'auto'}}>
+            <Card shadow={0} style={{
+                width: '100%',
+                margin: 'auto'
+            }}>
                 <CardTitle>Twój profil</CardTitle>
                 <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
+                    <h5 className="no-margin">Płeć</h5>
+                    <RadioGroup name="sex" value={this.state.sex}>
+                        <Radio value="female" className="margin-right-50">Kobieta</Radio>
+                        <Radio value="male">Mężczyzna</Radio>
+                    </RadioGroup>
+                    <h5 className="no-margin">Waga</h5>
+                    <TextField
+                        pattern="^[0-9]+$"
+                        validationMessage="Waga musi być liczbą całkowitą"
+                        label="Podaj swoją wagę w kilogramach"
+                        value={this.state.weight.value}
+                        onChange={(value) => this.onWeightChange(value)}
+                    />
+                    <h5 className="no-margin">Wzrost</h5>
+                    <TextField
+                        pattern="^[0-9]+$"
+                        validationMessage="Wzrost musi być liczbą całkowitą"
+                        label="Podaj swój wzrost w centymetrach"
+                        value={this.state.height.value}
+                        onChange={(value) => this.onHeightChange(value)}
+                    />
                 </CardText>
             </Card>
         );
