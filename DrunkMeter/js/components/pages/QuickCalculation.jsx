97d69dff2ffc742155336c17d23c3a33c3c2e9ec
Grzegorz Lada <grzegorz.lada@gmail.com>
2017-03-11 11:25:25 +0100

    Detaching test class from page.

--- DrunkMeter/js/components/pages/QuickCalculation.jsx
+++ DrunkMeter/js/components/pages/QuickCalculation.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Test from '../../calculator/Test';
 
 export default class QuickCalculation extends React.Component {
     render() {
-        var test = new Test();
-        test.test();
         return (
             <div>Szybkie liczenie</div>
         )
