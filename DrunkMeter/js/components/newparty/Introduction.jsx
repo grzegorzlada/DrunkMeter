210cdb7efa844d0533040bdba637d82ab7de660a
Grzegorz Lada <grzegorz.lada@gmail.com>
2017-03-13 19:13:31 +0100

    New sections on New Party page

--- DrunkMeter/js/components/newparty/Introduction.jsx
+++ DrunkMeter/js/components/newparty/Introduction.jsx
@@ -3,9 +3,12 @@ import {
     Button,
     Card,
     CardTitle,
-    CardText
+    CardText,
+    Radio,
+    RadioGroup
 } from 'react-mdl';
 import { Link } from 'react-router';
+import TextField from '../inputs/TextField';
 
 const propTypes = {
     height: React.PropTypes.number.isRequired,
@@ -32,8 +35,22 @@ export default class Introduction extends Component {
             }}>
                 <CardTitle>Nowa impreza</CardTitle>
                 <CardText>
-                    <div>Twoje dane: {this.getSexString()}, {this.props.height}cm wzrostu, {this.props.weight}kg wagi.</div>
+                    <h5>Twoje dane</h5>
+                    {this.getSexString()}, {this.props.height}cm wzrostu, {this.props.weight}kg wagi.
                     <div className="align-right"><Link to="/settings"><Button accent>Zmień swoje dane</Button></Link></div>
+                    
+                    <h5 className="no-margin-bottom">Początek imprezy</h5>
+                    <TextField label="Godzina" validationMessage="Nieprawidłowy format godziny" value=" " />
+                    
+                    <h5 className="no-margin-bottom">Koniec imprezy</h5>
+                    <TextField label="Godzina" validationMessage="Nieprawidłowy format godziny" value=" " />
+                    
+                    <h5>Pożywienie</h5>
+                    <RadioGroup name="food-status" value="" onChange={(e) => { }}>
+                        <Radio value="empty" className="margin-right-50">Pusty żołądek</Radio>
+                        <Radio value="half" className="margin-right-50">W połowie pełny żołądek</Radio>
+                        <Radio value="full">Pełny żołądek</Radio>
+                    </RadioGroup>
                 </CardText>
             </Card>
         );
