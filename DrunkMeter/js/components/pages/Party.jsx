1084cd5f074f05f7f39484734d59da135229fef0
Grzegorz Lada <grzegorz.lada@gmail.com>
2017-04-23 12:44:43 +0200

    Basic party detail component

--- DrunkMeter/js/components/pages/Party.jsx
+++ DrunkMeter/js/components/pages/Party.jsx
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import { find } from 'lodash';
+import { Card, CardTitle, CardText } from 'react-mdl';
+import moment from 'moment';
 
 export default class Party extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            partiesLoaded: false,
+            party: null
+        };
+    }
+
+    componentWillMount() {
+        DRUNKMETER.DrunkMeterStore.PartiesStore.getParties(this._partiesRetrievedFromStore.bind(this));
+    }
+
+    _partiesRetrievedFromStore(parties) {
+        var party = find(parties.historical, { 'id': this.props.params.id });
+
+        this.setState({
+            partiesLoaded: true,
+            party: party
+        });
+    }
+
+    _renderError() {
+        return (
+            <div>Nie udało się załadować wybranej imprezy</div>
+        );
+    }
+
+    _renderLoading() {
+        return (
+            <div>Ładowanie danych...</div>
+        );
+    }
+
+    _renderParty() {
+
+    }
+
+    _getCardTitle() {
+        return this.state.partiesLoaded && typeof this.state.party !== 'undefined'
+            ? 'Impreza z dnia: ' + moment(this.state.party.date).format('DD.MM.YYYY')
+            : 'Historia imprezy';
+    }
+
+    _renderCardText() {
+        if (!this.state.partiesLoaded) {
+            return this._renderLoading();
+        }
+
+        if (typeof this.state.party === 'undefined') {
+            return this._renderError();
+        }
+
+        return this._renderParty();
+    }
+
     render() {
-        //this.props.params.id
         return (
             <div>
-                PARTY COMPONENT
+                <Card shadow={0} style={{
+                    width: '100%',
+                    margin: 'auto',
+                    marginBottom: 10
+                }}>
+                    <CardTitle>{this._getCardTitle()}</CardTitle>
+                    <CardText>
+                        {this._renderCardText()}
+                    </CardText>
+                </Card>
             </div>
         );
     }
